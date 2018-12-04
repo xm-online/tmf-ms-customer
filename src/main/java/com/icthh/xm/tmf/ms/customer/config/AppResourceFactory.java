113771efd944505ba90469e2ae9e786f71132e3a
Andrew Koidan <deathangel908@gmail.com>
2018-12-04 11:32:14 +0200

    Added lep permissions timelines

---
+++ src/main/java/com/icthh/xm/tmf/ms/customer/config/AppResourceFactory.java
@@ -0,0 +1,13 @@
+package com.icthh.xm.tmf.ms.customer.config;
+
+import com.sun.prism.ResourceFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppResourceFactory implements ResourceFactory {
+
+    @Override
+    public Object getResource(Object resourceId, String objectType) {
+        return null;
+    }
+}
--- src/main/java/com/icthh/xm/tmf/ms/customer/config/ApplicationProperties.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/config/ApplicationProperties.java
@@ -1,6 +1,12 @@
 package com.icthh.xm.tmf.ms.customer.config;
 
+import com.icthh.xm.commons.lep.TenantScriptStorage;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Properties specific to Customer.
@@ -9,6 +15,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+@Getter
+@Setter
 public class ApplicationProperties {
+    private String kafkaSystemTopic;
+    private String kafkaSystemQueue;
+    private boolean timelinesEnabled;
+    private final Lep lep = new Lep();
+
+    private List<String> tenantIgnoredPathList = Collections.emptyList();
+
+    @Getter
+    @Setter
+    public static class Lep {
+        private TenantScriptStorage tenantScriptStorage;
+        private String lepResourcePathPattern;
+    }
 
 }
