84886a5e43ced5ab248d905281f17706e7fa6a5a
yevheniimuzyka <evgeniy.muzyka@jbs.com.ua>
2020-01-17 09:56:10 +0200

    add custom privilege description

--- src/main/java/com/icthh/xm/tmf/ms/customer/web/rest/CustomerDelegate.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/web/rest/CustomerDelegate.java
@@ -3,6 +3,7 @@ package com.icthh.xm.tmf.ms.customer.web.rest;
 import com.codahale.metrics.annotation.Timed;
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
+import com.icthh.xm.commons.permission.annotation.PrivilegeDescription;
 import com.icthh.xm.tmf.ms.customer.api.CustomerApiDelegate;
 import com.icthh.xm.tmf.ms.customer.lep.keyresolver.ProfileKeyResolver;
 import com.icthh.xm.tmf.ms.customer.model.Customer;
@@ -21,6 +22,7 @@ public class CustomerDelegate implements CustomerApiDelegate {
     @LogicExtensionPoint(value = "RetrieveCustomer", resolver = ProfileKeyResolver.class)
     @PreAuthorize("hasPermission({'id': #id}, 'CUSTOMER.GET')")
     @Override
+    @PrivilegeDescription("Privilege to get a customer")
     public ResponseEntity<List<Customer>> retrieveCustomer(String id,
                                                            String profile,
                                                            String fields) {
