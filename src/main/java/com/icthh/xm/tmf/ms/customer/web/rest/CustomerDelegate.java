8ebea8128efc6e4334e602f0a53dfb829c3fccf4
Dmittry Tsarenko <dimatsarenko89@gmail.com>
2020-02-12 14:25:51 +0200

    change travis build script

--- src/main/java/com/icthh/xm/tmf/ms/customer/web/rest/CustomerDelegate.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/web/rest/CustomerDelegate.java
@@ -4,6 +4,7 @@ import com.codahale.metrics.annotation.Timed;
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
 
+import com.icthh.xm.commons.permission.annotation.PrivilegeDescription;
 import com.icthh.xm.tmf.ms.customer.api.CustomerApiDelegate;
 import com.icthh.xm.tmf.ms.customer.lep.keyresolver.ProfileKeyResolver;
 import com.icthh.xm.tmf.ms.customer.model.Customer;
@@ -22,6 +23,7 @@ public class CustomerDelegate implements CustomerApiDelegate {
     @Timed
     @LogicExtensionPoint(value = "RetrieveCustomer", resolver = ProfileKeyResolver.class)
     @PreAuthorize("hasPermission({'id': #id}, 'CUSTOMER.GET')")
+    @PrivilegeDescription("Privilege to get a customer")
     @Override
     public ResponseEntity<List<Customer>> retrieveCustomer(String id,
                                                            String profile,
