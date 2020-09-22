1fee940403cae62d6eeae1b07a89917f23e256cf
Viktor Khoroshko <victor.khoroshko@gmail.com>
2020-09-22 22:43:17 +0300

    feature(customer): OBM-2820 Patch customer. Add missing LepAppEventListenerConfiguration

--- src/main/java/com/icthh/xm/tmf/ms/customer/lep/XmMsLepProcessingApplicationListener.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/lep/XmMsLepProcessingApplicationListener.java
@@ -8,19 +8,19 @@ import static com.icthh.xm.tmf.ms.customer.lep.LepXmCustomerConstants.BINDING_SU
 import static com.icthh.xm.tmf.ms.customer.lep.LepXmCustomerConstants.BINDING_SUB_KEY_SERVICE_TENANT_CONFIG_SERICE;
 import static com.icthh.xm.tmf.ms.customer.lep.LepXmCustomerConstants.BINDING_SUB_KEY_TEMPLATE_JDBC;
 import static com.icthh.xm.tmf.ms.customer.lep.LepXmCustomerConstants.BINDING_SUB_KEY_TEMPLATE_REST;
+import static com.icthh.xm.tmf.ms.customer.lep.LepXmCustomerConstants.BINDING_SUB_KEY_XM_AUTHENTICATION_CONTEXT_HOLDER;
 
 import com.icthh.xm.commons.config.client.service.TenantConfigService;
 import com.icthh.xm.commons.lep.commons.CommonsExecutor;
 import com.icthh.xm.commons.lep.commons.CommonsService;
 import com.icthh.xm.commons.lep.spring.SpringLepProcessingApplicationListener;
 import com.icthh.xm.commons.permission.service.PermissionCheckService;
+import com.icthh.xm.commons.security.XmAuthenticationContextHolder;
 import com.icthh.xm.lep.api.ScopedContext;
-
 import com.icthh.xm.tmf.ms.customer.service.CustomerService;
+import com.icthh.xm.tmf.ms.customer.service.SeparateTransactionExecutor;
 import java.util.HashMap;
 import java.util.Map;
-
-import com.icthh.xm.tmf.ms.customer.service.SeparateTransactionExecutor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
@@ -40,6 +40,7 @@ public class XmMsLepProcessingApplicationListener extends SpringLepProcessingApp
     private final PermissionCheckService permissionCheckService;
     private final SeparateTransactionExecutor transactionExecutor;
     private final CustomerService customerService;
+    private final XmAuthenticationContextHolder xmAuthenticationContextHolder;
 
     @Override
     protected void bindExecutionContext(ScopedContext executionContext) {
@@ -48,7 +49,8 @@ public class XmMsLepProcessingApplicationListener extends SpringLepProcessingApp
         services.put(BINDING_SUB_KEY_SERVICE_TENANT_CONFIG_SERICE, tenantConfigService);
         services.put(BINDING_SUB_KEY_PERMISSION_SERVICE, permissionCheckService);
         services.put(BINDING_SUB_KEY_SERVICE_SEPARATE_TRANSACTION_EXECUTOR, transactionExecutor);
-        services.put("customerService", customerService);
+        services.put("customerService", customerService); //todo V: needed?
+        services.put(BINDING_SUB_KEY_XM_AUTHENTICATION_CONTEXT_HOLDER, xmAuthenticationContextHolder);
 
         executionContext.setValue(BINDING_KEY_COMMONS, new CommonsExecutor(commonsService));
         executionContext.setValue(BINDING_KEY_SERVICES, services);
