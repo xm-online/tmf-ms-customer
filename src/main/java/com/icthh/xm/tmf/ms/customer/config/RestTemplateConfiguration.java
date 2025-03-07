cbace278d873960970176b183f9849e15d43cdfd
pzadorovskyi <pzadorovskyi@itsmartflex.com>
2025-03-07 12:52:46 +0200

    MW21-64 fixed rest template configuration

--- src/main/java/com/icthh/xm/tmf/ms/customer/config/RestTemplateConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/config/RestTemplateConfiguration.java
@@ -1,7 +1,6 @@
 package com.icthh.xm.tmf.ms.customer.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
@@ -13,22 +12,23 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class RestTemplateConfiguration {
 
-    @Bean
     @ConditionalOnProperty(
         value = {"spring.cloud.loadbalancer.enabled"},
-        havingValue = "true"
+        havingValue = "true",
+        matchIfMissing = true
     )
-    public RestTemplate customerRestTemplate(RestTemplateCustomizer customizer) {
-        log.info("customerRestTemplate creation with loadBalancer started");
+    @Bean("customerRestTemplate")
+    public RestTemplate restTemplateWithLoadBalancer(RestTemplateCustomizer customizer) {
+        log.info("restTemplateWithLoadBalancer creation with loadBalancer started");
         RestTemplate restTemplate = new RestTemplate();
         customizer.customize(restTemplate);
         return restTemplate;
     }
 
-    @Bean
-    @ConditionalOnMissingBean
-    public RestTemplate customerRestTemplate() {
-        log.info("customerRestTemplate creation without loadBalancer started");
+    @Bean("customerRestTemplate")
+    @ConditionalOnMissingBean(name = "customerRestTemplate")
+    public RestTemplate vanillaRestTemplate() {
+        log.info("vanillaRestTemplate creation without loadBalancer started");
         return new RestTemplate();
     }
 }
