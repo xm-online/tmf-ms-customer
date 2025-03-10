6cca0b6fd638268e21dc42236964f2a35d8b776f
pzadorovskyi <pzadorovskyi@itsmartflex.com>
2025-03-10 17:40:56 +0200

    MW21-64 moved to commons 3.2.14

--- src/main/java/com/icthh/xm/tmf/ms/customer/config/DynamicConsumerConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/config/DynamicConsumerConfiguration.java
@@ -3,18 +3,15 @@ package com.icthh.xm.tmf.ms.customer.config;
 import com.icthh.xm.commons.config.client.repository.TenantListRepository;
 import com.icthh.xm.commons.topic.service.DynamicConsumerConfigurationService;
 import com.icthh.xm.commons.topic.service.TopicManagerService;
-import com.icthh.xm.tmf.ms.customer.service.dto.RefreshDynamicConsumersEvent;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @DependsOn("topicConfigurationService")
 @Component
 @Slf4j
-public class DynamicConsumerConfiguration extends DynamicConsumerConfigurationService implements
-    ApplicationListener<RefreshDynamicConsumersEvent> {
+public class DynamicConsumerConfiguration extends DynamicConsumerConfigurationService {
 
 
     public DynamicConsumerConfiguration(
@@ -22,11 +19,5 @@ public class DynamicConsumerConfiguration extends DynamicConsumerConfigurationSe
         TopicManagerService topicManagerService,
         TenantListRepository tenantListRepository) {
         super(dynamicConsumerConfigurations, topicManagerService, tenantListRepository);
-    }
-
-    @Override
-    public void onApplicationEvent(RefreshDynamicConsumersEvent event) {
-        log.debug("OnApplicationEvent with event = {}", event);
-        refreshDynamicConsumers(event.getTenantKey());
     }
 }
