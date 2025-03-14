791f8290b45ea53cbef8a97e3e05df5f6466e1c6
pzadorovskyi <pzadorovskyi@itsmartflex.com>
2025-03-14 13:06:22 +0200

    MW21-64 fixed profile resolver for cases when profile is not mandatory

--- src/main/java/com/icthh/xm/tmf/ms/customer/lep/keyresolver/ProfileKeyResolver.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/lep/keyresolver/ProfileKeyResolver.java
@@ -1,14 +1,15 @@
 package com.icthh.xm.tmf.ms.customer.lep.keyresolver;
 
 import static java.util.Optional.ofNullable;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 import com.icthh.xm.lep.api.LepKeyResolver;
 import com.icthh.xm.lep.api.LepMethod;
 import java.util.List;
+import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -22,19 +23,21 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class ProfileHeaderKeyResolver implements LepKeyResolver {
-
-    private final ProfileKeyResolver profileKeyResolver;
+public class ProfileKeyResolver implements LepKeyResolver {
 
     @Override
     public List<String> segments(LepMethod method) {
+        String profile = getProfile(method);
+        return List.of(Optional.ofNullable(profile).orElse(EMPTY));
+    }
+
+    private String getProfile(LepMethod method) {
         HttpServletRequest request =
             ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         String profile = ofNullable(request.getHeader("Profile"))
+            .or(() -> ofNullable(method.getParameter("profile", String.class)))
             .orElse(Strings.EMPTY);
-        if (StringUtils.isBlank(profile)) {
-            return List.of(Strings.EMPTY);
-        }
-        return profileKeyResolver.segments(method);
+        log.info("getProfile: result: {}", profile);
+        return profile;
     }
 }
--- src/main/java/com/icthh/xm/tmf/ms/customer/web/rest/CustomerDelegate.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/web/rest/CustomerDelegate.java
@@ -1,23 +1,23 @@
 package com.icthh.xm.tmf.ms.customer.web.rest;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 import com.codahale.metrics.annotation.Timed;
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
 import com.icthh.xm.commons.permission.annotation.PrivilegeDescription;
 import com.icthh.xm.tmf.ms.customer.api.CustomerApiDelegate;
 import com.icthh.xm.tmf.ms.customer.lep.keyresolver.ProfileHeaderKeyResolver;
+import com.icthh.xm.tmf.ms.customer.lep.keyresolver.ProfileKeyResolver;
 import com.icthh.xm.tmf.ms.customer.model.Customer;
 import com.icthh.xm.tmf.ms.customer.model.CustomerCreate;
 import com.icthh.xm.tmf.ms.customer.model.PatchOperation;
 import com.icthh.xm.tmf.ms.customer.service.CustomerService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-import static org.springframework.http.ResponseEntity.ok;
 
 @Component
 @LepService(group = "service")
@@ -27,13 +27,13 @@ public class CustomerDelegate implements CustomerApiDelegate {
     private final CustomerService customerService;
 
     @Timed
-    @LogicExtensionPoint(value = "RetrieveCustomer", resolver = ProfileHeaderKeyResolver.class)
+    @LogicExtensionPoint(value = "RetrieveCustomer", resolver = ProfileKeyResolver.class)
     @PreAuthorize("hasPermission({'id': #id, 'profile': #profile}, 'CUSTOMER.GET')")
     @PrivilegeDescription("Privilege to get a customer")
     @Override
     public ResponseEntity<List<Customer>> retrieveCustomer(String id,
-                                                           String profile,
-                                                           String fields) {
+        String profile,
+        String fields) {
         return ok(customerService.getCustomer(id, profile, fields));
     }
 
@@ -47,7 +47,7 @@ public class CustomerDelegate implements CustomerApiDelegate {
     }
 
     @Timed
-    @LogicExtensionPoint(value = "RetrieveCustomer", resolver = ProfileHeaderKeyResolver.class)
+    @LogicExtensionPoint(value = "RetrieveCustomer", resolver = ProfileKeyResolver.class)
     @PreAuthorize("hasPermission({'id': #id, 'profile': #profile}, 'CUSTOMER.GET')")
     @PrivilegeDescription("Privilege to get a customers list")
     @Override
