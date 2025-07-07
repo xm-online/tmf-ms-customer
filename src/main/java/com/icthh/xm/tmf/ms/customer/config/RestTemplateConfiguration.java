a1975669ac34a8883db1ae20c3e11e54d6f6d8db
pzadorovskyi <pzadorovskyi@itsmartflex.com>
2025-07-07 10:22:10 +0300

    added timeout configuration for customer

--- src/main/java/com/icthh/xm/tmf/ms/customer/config/RestTemplateConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/customer/config/RestTemplateConfiguration.java
@@ -1,8 +1,12 @@
 package com.icthh.xm.tmf.ms.customer.config;
 
+import com.icthh.xm.commons.config.client.config.XmTimeoutProperties;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,17 +22,38 @@ public class RestTemplateConfiguration {
         matchIfMissing = true
     )
     @Bean("customerRestTemplate")
-    public RestTemplate restTemplateWithLoadBalancer(RestTemplateCustomizer customizer) {
+    public RestTemplate restTemplateWithLoadBalancer(RestTemplateBuilder restTemplateBuilder,
+        RestTemplateCustomizer customizer,
+        ObjectProvider<XmTimeoutProperties> timeoutPropertiesProvider) {
         log.info("restTemplateWithLoadBalancer creation with loadBalancer started");
-        RestTemplate restTemplate = new RestTemplate();
+
+        RestTemplate restTemplate = createRestTemplate(restTemplateBuilder, timeoutPropertiesProvider);
+
         customizer.customize(restTemplate);
         return restTemplate;
     }
 
     @Bean("customerRestTemplate")
     @ConditionalOnMissingBean(name = "customerRestTemplate")
-    public RestTemplate vanillaRestTemplate() {
+    public RestTemplate vanillaRestTemplate(RestTemplateBuilder restTemplateBuilder,
+        ObjectProvider<XmTimeoutProperties> timeoutPropertiesProvider) {
         log.info("vanillaRestTemplate creation without loadBalancer started");
-        return new RestTemplate();
+        return createRestTemplate(restTemplateBuilder, timeoutPropertiesProvider);
+    }
+
+    private RestTemplate createRestTemplate(RestTemplateBuilder restTemplateBuilder,
+        ObjectProvider<XmTimeoutProperties> timeoutPropertiesProvider) {
+
+        XmTimeoutProperties timeoutProperties = timeoutPropertiesProvider.getIfAvailable();
+
+        if (Objects.nonNull(timeoutProperties)) {
+            log.info("customerCreateRestTemplate with timeouts={}", timeoutProperties);
+            return restTemplateBuilder
+                .setConnectTimeout(timeoutProperties.getConnectionTimeout())
+                .setReadTimeout(timeoutProperties.getReadTimeout())
+                .build();
+        }
+        log.info("customerCreateRestTemplate without timeouts");
+        return restTemplateBuilder.build();
     }
 }
